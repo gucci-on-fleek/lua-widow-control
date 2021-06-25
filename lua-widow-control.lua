a76e4dc15f4709288a2cb87e69a14ae46953b9d8
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2021-06-24 22:03:03 -0600

    Reformat files

--- lua-widow-control.lua
+++ lua-widow-control.lua
@@ -1,82 +1,145 @@
-lwc = {} -- Lua Widow Control
+lwc = {}
+lwc.name = "lua-widow-control"
 
-lwc.paragraphs = {}
-lwc.emergency_stretch = tex.sp("10em")
-lwc.max_demerits = 10000
+luatexbase.provides_module{
+    name = lwc.name,
+    date = "2021/06/24",
+    version = "v0.00",
+    description = [[
+        This module provides a LuaTeX-based solution to prevent
+        widows and orphans from appearing in a document. It does
+        so by increasing or decreasing the lengths of previous
+        paragraphs.
+    ]]
+}
+
+lwc.paragraphs = {} -- List to hold the alternate paragraph versions
+lwc.emergency_stretch = tex.sp("10em") -- \emergencystretch value for adjusted paragraphs
+lwc.max_demerits = 10000 -- Demerits assigned when a paragraph can't adjusted
 lwc.club_penalty = tex.clubpenalty
 lwc.widow_penalty = tex.widowpenalty
 lwc.attribute = luatexbase.new_attribute("lua-widow-control")
 
-function lwc.linebreaker(head)
-    local natural_node, natural_info = tex.linebreak(node.copy_list(head))
-    local tight_node, tight_info = tex.linebreak(node.copy_list(head), {looseness = -1, emergencystretch = lwc.emergency_stretch})
-    local loose_node, loose_info = tex.linebreak(node.copy_list(head), {looseness = 1, emergencystretch = lwc.emergency_stretch})
+if lwc.club_penalty == lwc.widow_penalty then
+    luatexbase.module_warning(lwc.name, [[
+        \clubpenalty and \widowpenalty both have the same value.
+        This will prevent the package from distinguishing between
+        orphans and widows and will almost certainly lead to
+        undesirable behavior.
+        ]])
+end
+
+
+function lwc.linebreak(head, params)
+    local new_head = node.copy_list(head)
+
+    -- Prevent ultra-short last lines (TeXBook p. 104)
+    local parfillskip = node.slide(new_head)
+    parfillskip.stretch_order = 0
+    parfillskip.stretch = 0.9 * tex.hsize
+
+    return tex.linebreak(new_head, params)
+end
+
+
+function lwc.save_paragraphs(head)
+    -- Produce the regular paragraph
+    local natural_node, natural_info = lwc.linebreak(head)
+
+    -- Prevent the "underfull hbox" warnings when we store a potential paragraph
+    luatexbase.add_to_callback("hpack_quality", function() end, "disable-box-warnings")
+
+    local short_node, short_info = lwc.linebreak(head, {
+        looseness = -1,
+        emergencystretch = lwc.emergency_stretch
+    })
+    local long_node, long_info = lwc.linebreak(head, {
+        looseness = 1,
+        emergencystretch = lwc.emergency_stretch
+    })
+    luatexbase.remove_from_callback("hpack_quality", "disable-box-warnings")
 
     local loose_demerits, tight_demerits, demerits, best_node
 
-    if tight_info.looseness == 0 then
+    -- If we can't change the length of a paragraph, assign a very large demerit value
+    if short_info.looseness == 0 then
         tight_demerits = lwc.max_demerits
     else
-        tight_demerits = tight_info.demerits
+        tight_demerits = short_info.demerits
     end
 
-    if loose_info.looseness == 0 then
+    if long_info.looseness == 0 then
         loose_demerits = lwc.max_demerits
     else
-        loose_demerits = loose_info.demerits
+        loose_demerits = long_info.demerits
     end
 
+    -- Only save the node with the least badness
     if loose_demerits < tight_demerits then
         demerits = loose_demerits
-        best_node = loose_node
+        best_node = long_node
     else
         demerits = tight_demerits
-        best_node = tight_node 
+        best_node = short_node
     end
 
     table.insert(lwc.paragraphs, {demerits = demerits, node = best_node, lines = natural_info.prevgraf})
 
+    -- Set attributes on the first and last node of the original paragraph so that
+    -- we can find and remove it later.
     node.set_attribute(natural_node, lwc.attribute, #lwc.paragraphs)
     node.set_attribute(node.slide(natural_node), lwc.attribute, -1 * #lwc.paragraphs)
 
-    tex.prevdepth = natural_info.prevdepth
+    tex.prevdepth = natural_info.prevdepth -- https://tex.stackexchange.com/a/403813
+
     return natural_node
 end
 
-function lwc.fix_orphans_widows(head)
-    local head_save = head
+
+function lwc.remove_widows(head)
+    local head_save = head -- Save the head to return at the end
     local penalty = tex.outputpenalty
     local paragraphs = lwc.paragraphs
 
     if penalty ~= lwc.club_penalty and penalty ~= lwc.widow_penalty then
+        -- We only need to process paragraphs with orphans or widows
         return head_save
     end
 
-    local key = 1
-    local min = paragraphs[key].demerits
+    local paragraph_index = 1
+    local minimum_demerits = paragraphs[paragraph_index].demerits
 
-    for k, v in pairs(paragraphs) do
-        if paragraphs[k].demerits < min then
-            key, min = k, v.demerits
+    for i, x in pairs(paragraphs) do
+        if paragraphs[i].demerits < minimum_demerits then
+            paragraph_index, minimum_demerits = i, x.demerits
         end
     end
 
-    local target_node = paragraphs[key].node
+    local target_node = paragraphs[paragraph_index].node
 
     while head do
-        if node.has_attribute(head, lwc.attribute, key) then
+        if node.has_attribute(head, lwc.attribute, paragraph_index) then
+            -- Insert the start of the replacement paragraph
             head.prev.next = target_node
         end
-        if node.has_attribute(head, lwc.attribute, -1 * key) then
+
+        if node.has_attribute(head, lwc.attribute, -1 * paragraph_index) then
+            -- Insert the end of the replacement paragraph
             node.slide(target_node).next = head.next
         end
+
+        -- Start of final paragraph
         if node.has_attribute(head, lwc.attribute, #paragraphs) then
             if penalty == lwc.club_penalty then
+                -- Move last line to next page
                 node.write(paragraphs[#paragraphs].node)
                 head.prev.next = nil
+
             elseif penalty == lwc.widow_penalty then
+                -- Insert last line on top of next page
                 local last_line = node.copy_list(head.next.next)
                 node.slide(last_line).next = node.copy_list(tex.lists.contrib_head)
+
                 head.next.next = nil
                 tex.lists.contrib_head = nil
                 tex.lists.contrib_head = last_line
@@ -85,12 +148,13 @@ function lwc.fix_orphans_widows(head)
         head = head.next
     end
 
+    lwc.paragraphs = {} -- Clear paragraphs array at the end of the page
+
     return head_save
 end
 
 
-luatexbase.add_to_callback("pre_output_filter", lwc.fix_orphans_widows, "Expands or shrinks a paragraph on the page to remove orphans and widows.")
-luatexbase.add_to_callback("linebreak_filter", lwc.linebreaker, "Takes over paragraph formation to save paragraphs with different lengths.")
-luatexbase.add_to_callback("hpack_quality", function() end, "hpack_quality")
+luatexbase.add_to_callback("pre_output_filter", lwc.remove_widows, "remove-widows")
+luatexbase.add_to_callback("linebreak_filter", lwc.save_paragraphs, "save-paragraphs")
 
 return lwc
