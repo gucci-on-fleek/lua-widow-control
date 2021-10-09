946449925959fa3adbad1bb79b725b7ee0eb47af
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2021-10-09 17:01:50 -0600

    Add version updating script

---
+++ update-version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# lua-widow-control
+# https://github.com/gucci-on-fleek/lua-widow-control
+# SPDX-License-Identifier: MPL-2.0+
+# SPDX-FileCopyrightText: 2021 gucci-on-fleek
+
+grep --exclude-dir=.git/ -Erl '%%[v]ersion' | xargs sed -i "/%%[v]ersion/ s/[[:digit:]]\.[[:digit:]]\.[[:digit:]]/$1/"
+
+grep --exclude-dir=.git/ -Erl '%%[d]ate' | xargs sed -Ei "/%%[d]ate/ s/[[:digit:]]{4}.[[:digit:]]{2}.[[:digit:]]{2}/$(date -I)/"
+
+sed -i '/%%date/ s|-|/|g; /%%date/ s|//|--|' source/lua-widow-control.sty source/lua-widow-control.lua
